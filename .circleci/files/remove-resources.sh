1dbb18dbce991d433d56d4dd9c60df0aae54ad72
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2022-10-08 02:59:23 +0200

    Fixing cleanup job

---
+++ .circleci/files/remove-resources.sh
@@ -0,0 +1,20 @@
+export STACKS=($(
+  aws cloudformation list-stacks \
+  --query "StackSummaries[*].StackName" \
+  --stack-status-filter CREATE_COMPLETE \
+  --output text \
+))
+echo "Stack names: ${STACKS[@]}"
+export OldWorkflowID=$(curl --insecure https://kvdb.io/McRAzFezPEMhjfEApiab4d/old_workflow_id)
+
+if [[ "${STACKS[@]}" =~ "${OldWorkflowID}" ]]
+then
+  echo "Removing..."
+  aws s3 rm "s3://udapeople-${OldWorkflowID}" --recursive
+  aws cloudformation delete-stack --stack-name "udapeople-backend-${OldWorkflowID}"
+  aws cloudformation delete-stack --stack-name "udapeople-frontend-${OldWorkflowID}"
+  echo "Removed."
+else
+  echo "Unable to match OldWorkflowID ${OldWorkflowID} to any stack"
+  echo "Stacks: ${STACKS[@]}"
+fi
